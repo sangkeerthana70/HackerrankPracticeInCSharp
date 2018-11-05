e65e4d1708e852f8866a9f2fcf5e1ac8e5f0cdc0
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-11-04 19:04:11 -0500

    Initial commit

---
+++ CountingValleys/CountingValleys/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace CountingValleys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int steps = 8;
+            string s = "DDUUUUDD";
+
+            int result = countingValleys(steps, s);
+        }
+
+        static int countingValleys(int steps, string s)
+        {
+            int valleyCount = 0;
+            int altitude = 0;
+            for(int i = 0; i < s.Length; i++)
+            {
+                if(s[i] == 'U')
+                {
+                    Console.WriteLine("Climb up hill");
+                    altitude++;
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Climb downhill");
+                    altitude--;
+                    if(altitude == -1)
+                    {
+                        Console.WriteLine("Hit sea level");
+                        valleyCount += 1;
+                    }
+                }
+                Console.WriteLine("Altitude: " + altitude);
+                
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Valley Count: " + valleyCount);
+            return valleyCount;
+        }
+    }
+}
